bbf168bd02834866d405b18073f01b95b4729cab
simultech <simultech@gmail.com>
2016-01-11 22:34:21 +1000

    Functionality working :)

--- ext/src/inject/embed.js
+++ ext/src/inject/embed.js
@@ -1,16 +1,27 @@
 var searchText = [];
 var captionsURL = ytplayer.config.args.ttsurl + "&kind=asr&lang=en&fmt=srv1";
 
+var lastTerm = "";
 
 function doSearch() {
+	var found = false;
 	var searchTerm = $('#ytsearch').val();
+	if(searchTerm == lastTerm) {
+		return;
+	}
+	lastTerm = searchTerm;
 	for (var i in searchText) {
 		if (searchText[i].text.indexOf(searchTerm) > -1) {
-			var startTime = parseInt(searchText[i].start) - 0.05;
-			yt.player.getPlayerByElement("player-api").seekTo(startTime);
-			yt.player.getPlayerByElement("player-api").playVideo();
-			console.log(searchText[i]);
-			break;
+			if (!found) {
+				var startTime = parseInt(searchText[i].start) - 0.05;
+				yt.player.getPlayerByElement("player-api").seekTo(startTime);
+				yt.player.getPlayerByElement("player-api").playVideo();
+				console.log(searchText[i]);
+				found = true;
+			}
+			searchText[i].el.css({'visibility':'visible'});
+		} else {
+			searchText[i].el.css({'visibility':'hidden'});
 		}
 	}
 }
@@ -24,11 +35,11 @@ function initialize() {
 		url: captionsURL,
 		dataType: "xml",
 		success: function(data) {
-	  		captions = $(data).find('transcript').find('text');
-	  		for(var i=0; i<captions.length; i++) {
-	  			searchText.push({text: captions[i].innerHTML.toLowerCase(), start: captions[i].getAttribute('start'), duration: captions[i].getAttribute('dur')});
-	  		}
+
+			var width = 850;
+
     		var ytsearch = $('<input type="text" id="ytsearch" placeholder="Instant Search" />');
+
     		ytsearch.css({
     			'font-size': '140%',
     			'padding': '4px',
@@ -40,6 +51,61 @@ function initialize() {
     		ytsearch.keyup(function() {
       			doSearch();
     		});
+
+			var searchProgressBar = $('<div></div>');
+			searchProgressBar.css({
+				'background-color': 'white',
+				'height': '10px',
+				'width': width+'px',
+				'border': '1px solid #999',
+				'margin': '20px auto',
+    			'display': 'block',
+    			'margin-top': '-11px',
+    			'position': 'relative'
+			});
+
+
+			searchProgressBar.insertAfter($("#placeholder-player"));
+
+			captions = $(data).find('transcript').find('text');
+	  		for(var i=0; i<captions.length; i++) {
+	  			var duration = yt.player.getPlayerByElement("player-api").getDuration();
+	  			var xpos = parseFloat(captions[i].getAttribute('start'))/parseFloat(duration) * width;
+	  			var length = parseFloat(captions[i].getAttribute('dur'))/parseFloat(duration) * width;
+	  			var el = $('<div></div>');
+	  			el.css({
+	  				'background-color': '#fdd',
+	  				'width': length+'px',
+	  				'left': xpos+'px',
+	  				'position':'absolute',
+	  				'height':'10px',
+	  				'visibility':'hidden'
+	  			});
+	  			el.data('start',captions[i].getAttribute('start'));
+	  			el.click(function() {
+	  				yt.player.getPlayerByElement("player-api").seekTo($(this).data('start'));
+	  			});
+	  			searchText.push({el: el, text: captions[i].innerHTML.toLowerCase(), start: captions[i].getAttribute('start'), duration: captions[i].getAttribute('dur')});
+	  			searchProgressBar.append(el);
+	  		}
+
+	  		var searchProgressBarCurrentTime = $('<div></div>');
+	  		searchProgressBarCurrentTime.css({
+	  			'background-color': 'red',
+	  			'width': '2px',
+	  			'height': '10px',
+	  			'left': '0px',
+	  			'position': 'absolute'
+	  		});
+	  		searchProgressBar.append(searchProgressBarCurrentTime);
+
+	  		setInterval(function() {
+	  			var currentTime = yt.player.getPlayerByElement("player-api").getCurrentTime();
+	  			var duration = yt.player.getPlayerByElement("player-api").getDuration();
+	  			var xpos = parseFloat(currentTime)/parseFloat(duration) * width;
+	  			searchProgressBarCurrentTime.css({'left': xpos});
+	  		},200);
+
     		console.log("Loaded");
   		}
 	});
